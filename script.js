5b13539ac622cac10cd9621f412a2a0f1ea0efb4
owais-dev24 <owais03361214695@gmail.com>
2025-10-31 06:14:02 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+alert("Welcome to my website")
+alert("Error!Please enter a valid password")
+alert("Welcome to JS Land...\nHappy coding!")
+alert("Hello... I can run JS through my web browser's console");
+function showAlert() {
+  alert("You clicked the button! JavaScript alert is working perfectly.");
+}
